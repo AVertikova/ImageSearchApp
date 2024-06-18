59d34c953fbdfa541a82bfd0a8ad22a1dd6ad891
Anna Vertikova <an.vertikova@gmail.com>
2024-06-18 11:33:44 +0700

    Project base settings

---
+++ ImageSearchApp/ImageSearchApp/App/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ImageSearchApp
+//
+//  Created by Анна Вертикова on 18.06.2024.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
